6387736fc8a1ecbb179ff316788b0936d68cb6bf
Whitney Muchira <whitneymuchira@gmail.com>
2023-05-28 14:29:19 +0300

    changed image source

--- Routes/Home.jsx
+++ Routes/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../src/Components/Hero'
 import Destination from '../src/Components/Destination'
 import Trips from '../src/Components/Trips'
 import Footer from '../src/Components/Footer'
+import HeroImage from 'src/assets/Images/neom-aWHKsYkbCi8-unsplash.jpg'
 
 function Home() {
   return (
@@ -13,7 +14,7 @@ function Home() {
       cName="hero-text"
       title="Not all who wander are lost"
       text="Choose Your Favourite Destinations"
-      heroImg="src/assets/Images/neom-aWHKsYkbCi8-unsplash.jpg" 
+      heroImg={HeroImage} 
       heroAlt="alternative img"
       url="/about"
       btnText="Travel Plans"
